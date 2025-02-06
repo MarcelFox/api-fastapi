7c0789777412f490a2fa1cf6157342321133df05
MarcelFox <marcelfox@live.com>
2025-02-06 20:55:32 -0300

    adding MongoRepository

---
+++ src/shared/repository/mongo_repository.py
@@ -0,0 +1,34 @@
+from typing import TypeVar, List, Optional
+from src.shared.abstract import AbstractRepository
+
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.database import Database
+
+T = TypeVar('T')
+
+class MongoRepository(AbstractRepository[T]):
+    def __init__(self, db_name: str, user: str, password:str, collection_name: str, host: str, port: str):
+        super().__init__(db_name, user, password, host, port)
+        self.client = MongoClient(f'mongodb+srv://{user}:{password}@{host}:{port}/{db_name}')
+        self.db: Database = self.client[db_name]
+        self.collection: Collection = self.db[collection_name]
+    
+    def insert_one(self, entity: T) -> str:
+        return str(self.collection.insert_one(entity).inserted_id)
+
+    def find_one(self, query: dict) -> Optional[T]:
+        return self.collection.find_one(query)
+
+    def find_all(self, query: dict = {}) -> List[T]:
+        return list(self.collection.find(query))
+
+    def update_one(self, query: dict, update: dict) -> int:
+        return self.collection.update_one(query, {'$set': update}).modified_count
+
+    def delete_one(self, query: dict) -> int:
+        return self.collection.delete_one(query).deleted_count
+
+    def close_connection(self):
+        """Close the database connection."""
+        self.client.close()
