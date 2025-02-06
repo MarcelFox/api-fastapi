1c9206b4b84f73127d96541a4db85187de121789
MarcelFox <marcelfox@live.com>
2025-02-06 17:14:46 -0300

    adjusting endpoint functions

--- src/app/reservations/routes.py
+++ src/app/reservations/routes.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/")
-async def hello():
+@router.post("/")
+async def new_reservation():
     return {"message": "Hello Reservations!"}
--- src/app/rooms/routes.py
+++ src/app/rooms/routes.py
@@ -3,5 +3,13 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/")
-async def hello():
+async def list_registered_rooms():
     return {"message": "Hello Rooms!"}
+
+@router.post("/")
+async def room_registration():
+    return {"message": "Room registered!"}
+
+@router.get("/{id}/availability")
+async def check_room_availability(id: int):
+    return {"message": f"Room {id} is available!"}
