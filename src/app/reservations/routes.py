ec947bde8a44cbe7716043d43db15300ce6c8036
MarcelFox <marcelfox@live.com>
2025-02-07 00:18:36 -0300

    adjusting routes

--- src/app/reservations/routes.py
+++ src/app/reservations/routes.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
+from src.app.reservations.controller import ReservationsController
+from src.app.reservations.repository import Reservation
+
 router = APIRouter()
+controller = ReservationsController()
 
 @router.post("/")
-async def new_reservation():
-    return {"message": "Hello Reservations!"}
+async def new_reservation(reservation: Reservation):
+    return controller.create_new_room(reservation)
--- src/app/rooms/routes.py
+++ src/app/rooms/routes.py
@@ -1,19 +1,20 @@
 from typing import List
 from fastapi import APIRouter
-from .controller.room_controller import RoomController
-from .repository.room_repository import Room
+from .controller import RoomController
+from .repository import Room
+from src.app.reservations.repository import Reservation
 
 router = APIRouter()
-controller = RoomController()
+room_controller = RoomController()
 
 @router.get("/")
 async def list_registered_rooms() -> List[Room]:
-    return controller.list_all_registered_rooms()
+    return room_controller.list_all_registered_rooms()
 
 @router.post("/")
 async def room_registration(room: Room) -> str:
-    return controller.create_new_room(room)
+    return room_controller.create_new_room(room)
 
 @router.get("/{id}/availability")
-async def check_room_availability(id: int):
-    return {"message": f"Room {id} is available!"}
+async def check_room_availability(id: str, start_time: str, end_time: str) -> dict:
+    return room_controller.list_rooms_availability(id, start_time, end_time)
