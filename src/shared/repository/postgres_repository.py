11aef37b5c19d7e22393f4aaa7c61dd52554d634
MarcelFox <marcelfox@live.com>
2025-02-07 15:56:53 -0300

    adjusting insert, find and find_all

--- src/shared/repository/postgres_repository.py
+++ src/shared/repository/postgres_repository.py
@@ -6,33 +6,37 @@ from sqlalchemy.future import select
 from src.shared.abstract.abstract_repository import AbstractRepository
 
 T = TypeVar('T')
+G = TypeVar('G')
 
 class PostgresRepository(AbstractRepository[T]):
-    def __init__(self, connection_url: str):
+    def __init__(self, connection_url: str, model: G):
+        self.model: G = model
         self.connection_url = connection_url
         self.engine = create_async_engine(self.connection_url, echo=True)
         self.SessionLocal = sessionmaker(
             bind=self.engine, class_=AsyncSession, expire_on_commit=False
         )
 
-    async def insert(self, entity) -> T | None:
+    async def insert(self, data: dict) -> T | None:
         async with self.SessionLocal() as session:
             async with session.begin():
+                entity: G = self.model(**data)
                 session.add(entity)
                 await session.flush()
                 await session.refresh(entity)
                 return entity
         return None
 
-    async def find(self, entity: T, id: str) -> Optional[T]:
+    async def find(self, data: dict) -> Optional[T]:
         async with self.SessionLocal() as session:
-            result = await session.execute(select(entity).filter(entity.id == id))
+            result = await session.execute(select(self.model).filter_by(**data))
             return result.scalars().first()
 
     async def find_all(self) -> List[T]:
         async with self.SessionLocal() as session:
-            result = await session.execute(select())
+            result = await session.execute(select(self.model))
             return result.scalars().all()
+        return None
 
     async def update(self, id: str, data: dict) -> T:
         async with self.SessionLocal() as session:
