687114f138153ebd6de809ee7397eab7124475da
MarcelFox <marcelfox@live.com>
2025-02-06 23:10:37 -0300

    adding mongo repository class

--- src/shared/repository/mongo_repository.py
+++ src/shared/repository/mongo_repository.py
@@ -8,9 +8,9 @@ from pymongo.database import Database
 T = TypeVar('T')
 
 class MongoRepository(AbstractRepository[T]):
-    def __init__(self, db_name: str, user: str, password:str, collection_name: str, host: str, port: str):
-        super().__init__(db_name, user, password, host, port)
-        self.client = MongoClient(f'mongodb+srv://{user}:{password}@{host}:{port}/{db_name}')
+    def __init__(self, db_name: str, user: str, password:str, collection_name: str, host: str = "0.0.0.0"):
+        super().__init__(db_name, user, password, host)
+        self.client = MongoClient(f'mongodb://{user}:{password}@{host}')
         self.db: Database = self.client[db_name]
         self.collection: Collection = self.db[collection_name]
     
@@ -21,7 +21,7 @@ class MongoRepository(AbstractRepository[T]):
         return self.collection.find_one(query)
 
     def find_all(self, query: dict = {}) -> List[T]:
-        return list(self.collection.find(query))
+        return list(self.collection.find({}))
 
     def update_one(self, query: dict, update: dict) -> int:
         return self.collection.update_one(query, {'$set': update}).modified_count
