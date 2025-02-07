e613628a466048042ba846898f86bb6fcd97a2ba
MarcelFox <marcelfox@live.com>
2025-02-07 08:39:10 -0300

    adjusting Room repository

--- src/app/rooms/repository.py
+++ src/app/rooms/repository.py
@@ -1,16 +1,13 @@
-from typing import Annotated, Optional, TypedDict
-from pydantic import BaseModel, BeforeValidator, Field
+from pydantic import BaseModel
 
-from src.shared.repository.mongo_repository import MongoRepository
-
-PyObjectId = Annotated[str, BeforeValidator(str)]
+from src.shared.repository import PostgresRepository
 
 class Room(BaseModel):
-    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    id: str
     name: str
     capacity: int
     location: str
 
-class RoomRepository(MongoRepository[Room]):
+class RoomRepository(PostgresRepository[Room]):
     def __init__(self):
         super().__init__(db_name='reservation_db', user="admin", password="password", collection_name='rooms')
