e4b1945adae1d82acbed7734356018962da166b5
MarcelFox <marcelfox@live.com>
2025-02-07 15:35:58 -0300

    adding load_models_plugin

---
+++ src/plugins/load_models.py
@@ -0,0 +1,16 @@
+import importlib
+from pathlib import Path
+
+def load_models_plugin():
+    """Dynamically load routes for each domain under app.
+    Router files must be named as 'src/app/DOMAIN/routes.py'.
+
+    Args:
+        app (FastAPI): FastAPI main instance.
+    """    
+    base_path = Path("src/app")
+    for route_file in base_path.rglob('**/model.py'):
+        module_path = route_file.relative_to(base_path).with_suffix('').as_posix()
+        module = importlib.import_module(f'src.app.{module_path.replace("/", ".")}', 'Base')
+        yield module.Base.metadata
+       
