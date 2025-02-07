f84e727e24cf660b103903c005058dc3835504b5
MarcelFox <marcelfox@live.com>
2025-02-06 23:11:19 -0300

    adjusting room routes

--- src/app/rooms/routes.py
+++ src/app/rooms/routes.py
@@ -1,14 +1,18 @@
+from typing import List
 from fastapi import APIRouter
+from .controller.room_controller import RoomController
+from .repository.room_repository import Room
 
 router = APIRouter()
+controller = RoomController()
 
 @router.get("/")
-async def list_registered_rooms():
-    return {"message": "Hello Rooms!"}
+async def list_registered_rooms() -> List[Room]:
+    return controller.list_all_registered_rooms()
 
 @router.post("/")
-async def room_registration():
-    return {"message": "Room registered!"}
+async def room_registration(room: Room) -> str:
+    return controller.create_new_room(room)
 
 @router.get("/{id}/availability")
 async def check_room_availability(id: int):
