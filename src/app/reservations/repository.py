2fd5eaf818df18f2fd209487aad6bdc97fafd521
MarcelFox <marcelfox@live.com>
2025-02-07 22:07:00 -0300

    adjusting repository classes

--- src/app/reservations/repository.py
+++ src/app/reservations/repository.py
@@ -1,18 +1,23 @@
-from typing import Annotated, Optional
+import os
 from datetime import datetime
-from pydantic import BaseModel, BeforeValidator, Field
+from pydantic import BaseModel
 
-from src.shared.repository.mongo_repository import MongoRepository
-
-PyObjectId = Annotated[str, BeforeValidator(str)]
+from src.app.reservations.model import ReservationModel
+from src.shared.repository import PostgresRepository
 
 class Reservation(BaseModel):
-    id: Optional[PyObjectId] = Field(alias="_id", default=None)
-    room_id: str
+    user_name: str = "John Doe"
+    start_time: datetime = "2025-01-22T14:00:00"
+    end_time: datetime = "2025-01-22T16:00:00"
+    room_id: int
+
+class ReservationResponse(BaseModel):
+    id: int
     user_name: str
     start_time: datetime
     end_time: datetime
+    room_id: int
 
-class ReservationRepository(MongoRepository[Reservation]):
+class ReservationRepository(PostgresRepository[Reservation]):
     def __init__(self):
-        super().__init__(db_name='reservation_db', user="admin", password="password", collection_name='reservations')
+        super().__init__(connection_url=os.getenv("POSTGRES_URL"), model=ReservationModel)
--- src/app/rooms/repository.py
+++ src/app/rooms/repository.py
@@ -1,13 +1,19 @@
+import os
 from pydantic import BaseModel
-
+from src.app.rooms.model import RoomModel
 from src.shared.repository import PostgresRepository
 
 class Room(BaseModel):
-    id: str
+    name: str
+    capacity: int
+    location: str
+
+class RoomResponse(BaseModel):
+    id: int
     name: str
     capacity: int
     location: str
 
 class RoomRepository(PostgresRepository[Room]):
     def __init__(self):
-        super().__init__(db_name='reservation_db', user="admin", password="password", collection_name='rooms')
+        super().__init__(connection_url=os.getenv('POSTGRES_URL'), model=RoomModel)
