c668e276d4251cc3ad1aa45bc7cd1833acb4e1c1
MarcelFox <marcelfox@live.com>
2025-02-08 01:45:52 -0300

    adjusting routes logic

--- src/app/reservations/routes.py
+++ src/app/reservations/routes.py
@@ -1,18 +1,37 @@
-from typing import List
+from datetime import datetime
+from typing import Annotated, List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer
 
 from src.app.reservations.controller import ReservationsController
 from src.app.reservations.repository import Reservation, ReservationResponse
+from src.app.token.routes import get_current_active_user
 
 router = APIRouter()
 controller = ReservationsController()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 @router.post("/")
-async def new_reservation(reservation: Reservation) -> ReservationResponse:
-    return await controller.create_new_room(vars(reservation))
+async def new_reservation(
+    reservation: Reservation, user: Annotated[str, Depends(get_current_active_user)]
+) -> ReservationResponse:
+    date_format = "%Y-%m-%dT%H:%M:%S"
+    wrong_time = reservation.start_time.strftime(
+        date_format
+    ) >= reservation.end_time.strftime(date_format)
+    if wrong_time:
+        raise HTTPException(status_code=406, detail="Wrong time.")
+    registered = reservation.user_name == user.full_name
+    if not registered:
+        raise HTTPException(status_code=400, detail="Reservation user not registered")
+    result = await controller.create_new_reservation(reservation)
+    if not result:
+        raise HTTPException(status_code=500, detail="Something went wrong.")
+    return result
 
 
 @router.get("/")
@@ -21,6 +40,13 @@ async def get_reservations() -> List[ReservationResponse]:
 
 
 @router.delete("/{id}")
-async def remove_reservation(id: int):
+async def remove_reservation(
+    id: int, user: Annotated[str, Depends(get_current_active_user)]
+):
+    found_user = await controller.find_user_by_reservation_id(id)
+    if not found_user:
+        raise HTTPException(status_code=404, detail="Entity not found")
+    if found_user != user.full_name:
+        raise HTTPException(status_code=401, detail="Not authorized")
     await controller.remove_reservation(id)
     return JSONResponse(content="", status_code=204)
--- src/app/rooms/routes.py
+++ src/app/rooms/routes.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from typing import List, Union
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from src.app.reservations.repository import ReservationResponse
 
@@ -23,9 +24,28 @@ async def room_registration(room: Room) -> RoomResponse:
 
 @router.get("/{id}/availability")
 async def check_room_availability(id: int, start_time: str, end_time: str) -> dict:
-    return await room_controller.list_rooms_availability(id, start_time, end_time)
+    try:
+        date_format = "%Y-%m-%dT%H:%M:%S"
+        wrong_time = datetime.strptime(start_time, date_format) >= datetime.strptime(
+            end_time, date_format
+        )
+        if wrong_time:
+            raise ValueError
+        return await room_controller.list_rooms_availability(id, start_time, end_time)
+    except ValueError:
+        raise HTTPException(status_code=406, detail="Wrong time")
 
 
 @router.get("/{id}/reservation")
-async def check_room_availability(id: int, date: str = "") -> List[ReservationResponse]:
-    return await room_controller.list_room_reservations(id, date)
+async def check_room_availability(
+    id: int, date: str = None
+) -> List[ReservationResponse]:
+    try:
+        if date:
+            datetime.strptime(date, "%Y-%m-%d")
+        result = await room_controller.list_room_reservations(id, date)
+        if not result:
+            raise HTTPException(status_code=404, detail="Room not found.")
+        return result
+    except ValueError:
+        raise HTTPException(status_code=406, detail="Wrong time format")
