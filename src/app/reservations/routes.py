4a8f56e445f9e7eee285f9b99b35fba06d503e42
MarcelFox <marcelfox@live.com>
2025-02-07 22:08:56 -0300

    imrpoving routes

--- src/app/reservations/routes.py
+++ src/app/reservations/routes.py
@@ -1,11 +1,22 @@
+from typing import List
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 from src.app.reservations.controller import ReservationsController
-from src.app.reservations.repository import Reservation
+from src.app.reservations.repository import Reservation, ReservationResponse
 
 router = APIRouter()
 controller = ReservationsController()
 
 @router.post("/")
-async def new_reservation(reservation: Reservation):
-    return controller.create_new_room(reservation)
+async def new_reservation(reservation: Reservation) -> ReservationResponse:
+    return await controller.create_new_room(vars(reservation))
+
+@router.get("/")
+async def get_reservations() -> List[ReservationResponse]:
+    return await controller.get_reservations()
+
+@router.delete("/{id}")
+async def remove_reservation(id: int):
+    await controller.remove_reservation(id)
+    return JSONResponse(content="", status_code=204)
--- src/app/rooms/routes.py
+++ src/app/rooms/routes.py
@@ -1,20 +1,24 @@
-from typing import List
+from typing import List, Union
 from fastapi import APIRouter
 from .controller import RoomController
-from .repository import Room
-from src.app.reservations.repository import Reservation
+from .repository import Room, RoomResponse
+from src.app.reservations.repository import ReservationResponse
 
 router = APIRouter()
 room_controller = RoomController()
 
 @router.get("/")
-async def list_registered_rooms() -> List[Room]:
-    return room_controller.list_all_registered_rooms()
+async def list_registered_rooms() -> List[RoomResponse]:
+    return await room_controller.list_all_registered_rooms()
 
 @router.post("/")
-async def room_registration(room: Room) -> str:
-    return room_controller.create_new_room(room)
+async def room_registration(room: Room) -> RoomResponse:
+    return await room_controller.create_new_room(vars(room))
 
 @router.get("/{id}/availability")
-async def check_room_availability(id: str, start_time: str, end_time: str) -> dict:
-    return room_controller.list_rooms_availability(id, start_time, end_time)
+async def check_room_availability(id: int, start_time: str, end_time: str) -> dict:
+    return await room_controller.list_rooms_availability(id, start_time, end_time)
+
+@router.get("/{id}/reservation")
+async def check_room_availability(id: int, date: str = '') -> List[ReservationResponse]:
+    return await room_controller.list_room_reservations(id, date)
