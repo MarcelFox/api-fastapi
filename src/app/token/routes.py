15d998ec55335d88548d16ebd48fd689f2fc18f3
MarcelFox <marcelfox@live.com>
2025-02-08 01:52:48 -0300

    adjusting to import fake_users_db

--- src/app/token/routes.py
+++ src/app/token/routes.py
@@ -4,22 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
+from src.config.fake_users_db import fake_users_db
 
 router = APIRouter()
 
