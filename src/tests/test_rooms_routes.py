00d94bde4dc5e00ca999701c9872dfaee17ad6df
Marcel Fox <marcelfox@live.com>
2025-02-09 16:16:07 -0300

    removing wrong docstring

--- src/tests/test_rooms_routes.py
+++ src/tests/test_rooms_routes.py
@@ -9,12 +9,6 @@ async def test_get_rooms(client):
 
 @pytest.mark.asyncio
 async def test_post_rooms(client, random_hash):
-    """test post method on '/rooms' route.
-
-    Args:
-        client (FastAPI): Api client fixture.
-        random_hash: Random hash string fixture.
-    """
     name = random_hash
     new_room = await client.post(
         "/rooms/", json={"name": name, "capacity": 0, "location": "Andar 1"}
