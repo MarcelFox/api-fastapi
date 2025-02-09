a6cec99485cb91ea3c68022c07e085df1749e35a
Marcel Fox <marcelfox@live.com>
2025-02-09 19:20:59 -0300

    adding create_database as fixture

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -16,32 +16,6 @@ TEST_DATABASE_NAME = "test_db"
 ADMIN_DATABASE_URL = "postgresql://user:password@localhost/postgres"
 TEST_DATABASE_URL = "postgresql+asyncpg://user:password@localhost/test_db"
 
-try:
-    conn = psycopg2.connect(ADMIN_DATABASE_URL)
-    conn.autocommit = True
-    cursor = conn.cursor()
-    cursor.execute(
-        "SELECT 1 FROM pg_database WHERE datname = %s", (TEST_DATABASE_NAME,)
-    )
-    exists = cursor.fetchone()
-    if not exists:
-        cursor.execute(f"CREATE DATABASE {TEST_DATABASE_NAME}")
-        alembic_cfg = Config("alembic.ini")
-        alembic_cfg.set_main_option("sqlalchemy.url", TEST_DATABASE_URL)
-        command.upgrade(alembic_cfg, "head")
-    conn.close()
-except psycopg2.errors.DuplicateDatabase:
-    print("Database already exists.")
-except psycopg2.errors.UniqueViolation as err:
-    print(err)
-
-
-@pytest_asyncio.fixture(scope="session", autouse=True)
-async def cleanup_test_db():
-    yield  # Wait until all tests are done
-
-    await drop_database()
-
 
 async def drop_database():
     loop = asyncio.get_running_loop()
@@ -60,6 +34,44 @@ def _drop_database_sync():
         connection.close()
     except Exception as e:
         print(f"Error dropping database: {e}")
+
+
+async def create_database():
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, _create_database_sync)
+
+
+def _create_database_sync():
+    try:
+        conn = psycopg2.connect(ADMIN_DATABASE_URL)
+        conn.autocommit = True
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT 1 FROM pg_database WHERE datname = %s", (TEST_DATABASE_NAME,)
+        )
+        exists = cursor.fetchone()
+        if not exists:
+            cursor.execute(f"CREATE DATABASE {TEST_DATABASE_NAME}")
+            alembic_cfg = Config("alembic.ini")
+            alembic_cfg.set_main_option("sqlalchemy.url", TEST_DATABASE_URL)
+            command.upgrade(alembic_cfg, "head")
+        conn.close()
+    except psycopg2.errors.DuplicateDatabase:
+        print("Database already exists.")
+    except psycopg2.errors.UniqueViolation as err:
+        print(err)
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def startup():
+    await create_database()
+    yield
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def teardown():
+    yield
+    await drop_database()
 
 
 @pytest_asyncio.fixture
