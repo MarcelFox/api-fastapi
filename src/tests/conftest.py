ae2bbfefa57ce0238dda392cee80608844df3b74
MarcelFox <marcelfox@live.com>
2025-02-09 15:29:14 -0300

    improving conftest to avoind errors

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,52 +1,40 @@
 """Test configuration module."""
 
-import asyncio
 import hashlib
 import os
 import secrets
 from typing import Any, AsyncGenerator
-import asyncpg
-import pytest
-import pytest_asyncio
-from sqlalchemy import create_engine
-from alembic.config import Config
-from alembic import command
-from httpx import ASGITransport, AsyncClient
-from sqlalchemy.ext.asyncio import create_async_engine
-from src.shared.classes.declarative_base import Base
-import asyncio
-import pytest_asyncio
-from sqlalchemy.ext.asyncio import create_async_engine
+
 import psycopg2
+import pytest_asyncio
+from alembic import command
+from alembic.config import Config
+from httpx import ASGITransport, AsyncClient
 
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     try:
-#         loop = asyncio.get_running_loop()
-#     except RuntimeError:
-#         loop = asyncio.new_event_loop()
-#     yield loop
-#     loop.close()
-
-ADMIN_DATABASE_URL = "postgresql://user:password@0.0.0.0/postgres"
 TEST_DATABASE_NAME = "test_db"
+ADMIN_DATABASE_URL = "postgresql://user:password@localhost/postgres"
+TEST_DATABASE_URL = "postgresql+asyncpg://user:password@localhost/test_db"
 
-@pytest_asyncio.fixture(scope='session', autouse=True)
-async def startup():
-    print('\n\nSTART SETUP FOR TESTS')
-    try:
-        conn = psycopg2.connect(ADMIN_DATABASE_URL)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", (TEST_DATABASE_NAME,))
-        exists = cursor.fetchone()
-        if not exists:
-            cursor.execute(f'CREATE DATABASE {TEST_DATABASE_NAME}')
-            alembic_cfg = Config("alembic.ini")
-            await asyncio.to_thread(command.upgrade, alembic_cfg, "head")
-        conn.close()
-    except psycopg2.errors.DuplicateDatabase:
-        print("Database already exists.")
+try:
+    conn = psycopg2.connect(ADMIN_DATABASE_URL)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT 1 FROM pg_database WHERE datname = %s", (TEST_DATABASE_NAME,)
+    )
+    exists = cursor.fetchone()
+    if not exists:
+        cursor.execute(f"CREATE DATABASE {TEST_DATABASE_NAME}")
+        alembic_cfg = Config("alembic.ini")
+        alembic_cfg.set_main_option(
+            "sqlalchemy.url", TEST_DATABASE_URL
+        )
+        command.upgrade(alembic_cfg, "head")
+    conn.close()
+except psycopg2.errors.DuplicateDatabase:
+    print("Database already exists.")
+except psycopg2.errors.UniqueViolation as err:
+    print(err)
 
 @pytest_asyncio.fixture
 async def random_hash() -> str:
@@ -54,8 +42,13 @@ async def random_hash() -> str:
     hash_object = hashlib.sha256(random_string.encode())
     return hash_object.hexdigest()
 
+
 @pytest_asyncio.fixture
 async def client() -> AsyncGenerator[Any, Any]:
+    os.environ["POSTGRES_URL"] = TEST_DATABASE_URL
     from src.main import app
-    async with AsyncClient(transport=ASGITransport(app), base_url="http://0.0.0.0") as test_client:
+
+    async with AsyncClient(
+        transport=ASGITransport(app), base_url="http://0.0.0.0"
+    ) as test_client:
         yield test_client
